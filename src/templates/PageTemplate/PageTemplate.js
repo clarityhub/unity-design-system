6648afa9aae0fca88ae72c37d98d89dfc8814cc0
Ivan Montiel <idmontie@gmail.com>
2019-03-04 21:35:33 -0700

    Fix page template

--- src/templates/PageTemplate/PageTemplate.js
+++ src/templates/PageTemplate/PageTemplate.js
@@ -1,9 +1,11 @@
 import React, { Fragment } from 'react';
 import styled from '@emotion/styled';
 
+import colors from '../../theme/colors';
 import Layout from '../../scaffolding/Layout';
 import { FlexGrid } from '../../scaffolding/FlexGrid';
 import Navbar from '../../components/Navbar';
+import { Card } from '../../components/Card';
 import Logo from '../../components/svgs/Logo';
 
 const SideNav = styled.div`
@@ -13,10 +15,16 @@ const SideNav = styled.div`
 // max width
 // overflow
 const Content = styled.div`
-    display: flex;
+    background-color: ${colors.muted};
     flex: 1;
     overflow: auto;
     max-height: 100%;
+
+    ${Card} {
+        margin: 2rem auto 0;
+        max-width: 820px;
+        width: 100%;
+    }
 `;
 
 const Page = ({ children, navItems }) => (
@@ -31,7 +39,9 @@ const Page = ({ children, navItems }) => (
             />
 
             <Content>
-                {children}
+                <Card>
+                    {children}
+                </Card>
             </Content>
         </FlexGrid>
     </Layout>  
