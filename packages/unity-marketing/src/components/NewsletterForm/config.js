66f68c4f0b8e61359a38dd0499bcc2868ba76756
Anna Loukianova <loukitzanna@gmail.com>
2019-06-04 19:47:57 -0700

    Use correct name for gdpr variable

--- packages/unity-marketing/src/components/NewsletterForm/config.js
+++ packages/unity-marketing/src/components/NewsletterForm/config.js
@@ -1,2 +1,2 @@
 export const URL = 'https://clarityhub.us18.list-manage.com/subscribe/post?u=7da525c17c8d5399e087120fd&amp;id=5b939ab943';
-export const GDPR = 'marketing_permissions';
+export const GDPR_NAME = 'marketing_permissions';
