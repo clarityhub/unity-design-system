f135e1df12c2517042fabac3dfd62cbe2386916b
Anna Loukianova <loukitzanna@gmail.com>
2019-06-04 21:04:49 -0700

    Update MailChimp address

--- packages/unity-marketing/src/components/NewsletterForm/config.js
+++ packages/unity-marketing/src/components/NewsletterForm/config.js
@@ -1,2 +1,2 @@
-export const URL = 'https://clarityhub.us18.list-manage.com/subscribe/post?u=7da525c17c8d5399e087120fd&amp;id=5b939ab943';
+export const URL = 'https://clarityhub.us18.list-manage.com/subscribe/post?u=7da525c17c8d5399e087120fd&amp;id=54d4089089';
 export const GDPR_NAME = 'marketing_permissions';
