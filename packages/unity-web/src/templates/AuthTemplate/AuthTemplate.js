ea3fe422ca9e8d0268489685e3ac9a3c8989e4a9
Ivan Montiel <idmontie@gmail.com>
2019-09-24 09:00:36 -0700

    Add login only button to auth template, add terms and privacy policy links

--- packages/unity-web/src/templates/AuthTemplate/AuthTemplate.js
+++ packages/unity-web/src/templates/AuthTemplate/AuthTemplate.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-
+import { Fragment } from 'react';
 import { css, jsx } from '@emotion/core';
 import styled from '@emotion/styled';
 import { arrayOf, node, shape, func } from 'prop-types';
@@ -18,12 +18,16 @@ const Section = styled.div`
     padding: 3rem;
     max-width: 520px;
 
+	display: flex;
+    flex-direction: column;
+    justify-content: center;
+
     ${Card} + ${Card} {
         margin-top: 1rem;
     }
 `;
 
-const AuthTemplate = ({ newsFeed, onSignUp, onLogin }) => (
+const AuthTemplate = ({ newsFeed, onSignUp, onLogin, onlyLogin = false, termsAndConditionsHref = "#", privacyPolicyHref = "#" }) => (
 	<Layout css={css`
         background-color: ${colors.dove.default};
     `}>
@@ -67,21 +71,46 @@ const AuthTemplate = ({ newsFeed, onSignUp, onLogin }) => (
 					</Typography>
                     
 					<Box margin={{ top: "small" }}>
-						<Button type="primary" block onClick={onSignUp}>
-                            Sign Up
-						</Button>
+						{
+							onlyLogin ? (
+								<Fragment>
+									<Button type="primary" outline block onClick={onLogin}>
+										Login
+									</Button>
+								</Fragment>
+							) : (
+								<Fragment>
+									<Button type="primary" block onClick={onSignUp}>
+										Sign Up
+									</Button>
 
-						<Typography center css={css`
-                            margin-top: 3rem;
-                            margin-bottom: 1rem;
-                        `}>
-                            Already have an account?
-						</Typography>
+									<Typography center css={css`
+										margin-top: 3rem;
+										margin-bottom: 1rem;
+									`}>
+										Already have an account?
+									</Typography>
 
-						<Button type="primary" outline block onClick={onLogin}>
-                            Login
-						</Button>
+									<Button type="primary" outline block onClick={onLogin}>
+										Login
+									</Button>
+								</Fragment>
+							)
+						}
 					</Box>
+					
+					<Typography center css={css`
+						color: #666;
+						font-size: 0.9rem;
+						margin-top: 3rem;
+						margin-bottom: 1rem;
+					`}>
+						By clicking on "Login" above, you acknowledge that you have read, understood, and agree to Clarity Hub's
+						{' '}
+						<a href={termsAndConditionsHref} target="_blank" rel="noopener noreferrer">Terms and Conditions</a> and
+						{' '}
+						<a href={privacyPolicyHref} target="_blank" rel="noopener noreferrer">Privacy Policy</a>.
+					</Typography>
 				</Section>
 			</FlexColumn>
 		</FlexGrid>
