93852a3bf0fbcadbee258926288dd495210a1129
Ivan Montiel <idmontie@gmail.com>
2020-12-19 14:09:58 -0700

    Wrap window reference

--- packages/unity-web/src/components/BackToTop/BackToTop.js
+++ packages/unity-web/src/components/BackToTop/BackToTop.js
@@ -25,30 +25,38 @@ const Wrapper = styled.div`
     `}
 `;
 
-const BackToTop = ({ icon, inline, scrollRef = window, scrollAmount = 100, alwaysShow = false, wrapperProps = {}, ...rest }) => {
+const BackToTop = ({ icon, inline, scrollRef, scrollAmount = 100, alwaysShow = false, wrapperProps = {}, ...rest }) => {
+	const scroll = scrollRef || (typeof window !== 'undefined' ? window: null);
+
 	const [hasScrolled, setHasScrolled] = useState(false);
 	const onClick = useCallback(() => {
-		if ('scrollRestoration' in window.history) {
-			window.history.scrollRestoration = 'manual';
-		}
+		if (typeof window !== 'undefined') {
+			if ('scrollRestoration' in window.history) {
+				window.history.scrollRestoration = 'manual';
+			}
 
-		window.scrollTo(0,0);
+			window.scrollTo(0,0);
+		}
 	}, []);
 
 	const onScroll = useCallback(() => {
-		if (scrollRef.scrollY > scrollAmount) {      
-			setHasScrolled(true);
-		} else if (scrollRef.scrollY < scrollAmount) {      
-			setHasScrolled(false);
-		}  
+		if (typeof scroll !== 'undefined') {
+			if (scroll.scrollY > scrollAmount) {      
+				setHasScrolled(true);
+			} else if (scroll.scrollY < scrollAmount) {      
+				setHasScrolled(false);
+			}
+		}
 	}, [setHasScrolled]);
 
 	useEffect(() => {
-		window.addEventListener('scroll', onScroll);
+		if (typeof window !== 'undefined') {
+			window.addEventListener('scroll', onScroll);
 
-		return () => {
-			window.removeEventListener('scroll', onScroll);
-		};
+			return () => {
+				window.removeEventListener('scroll', onScroll);
+			};
+		}
 	}, [onScroll]);
     
 	const wrapProps = {
